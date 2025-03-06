fcecf328b2fb310fecfa334ffced8339a62818df
oxyum services <29GariglioSe@fortcherry.org>
2025-03-05 19:18:07 -0500

    push

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ io.on('connection', socket => {
     });
 });
 
-// Start the server
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
     console.log('Server is running on http://localhost:3000');
 });
